765ca43e036690b1f59ce08e39ee2d7415a8ee50
zyf722 <1124982772@qq.com>
2024-04-04 20:24:38 +0800

    feat: docusaurus init

---
+++ docusaurus.config.ts
@@ -0,0 +1,87 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+    title: "Max's Notes",
+    tagline: "互联网角落里的一粒尘埃",
+    favicon: "img/favicon.ico",
+
+    // Set the production url of your site here
+    url: "https://your-docusaurus-site.example.com",
+
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: "/max-notes/",
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "zyf722", // Usually your GitHub org/user name.
+    projectName: "max-notes", // Usually your repo name.
+
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "zh-Hans",
+        locales: ["zh-Hans"],
+    },
+
+    presets: [
+        [
+            "classic",
+            {
+                docs: {
+                    sidebarPath: "./sidebars.ts",
+                    routeBasePath: "/"
+                },
+                blog: false,
+                theme: {
+                    customCss: "./src/css/custom.css",
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        navbar: {
+            title: "Max's Notes",
+            logo: {
+                alt: "Site Logo",
+                src: "img/logo.png",
+            },
+            items: [
+                {
+                    href: "https://zyf722.github.io",
+                    label: "Blog",
+                    position: "right"
+                },
+                {
+                    href: "https://github.com/zyf722/max-notes",
+                    position: "right",
+                    className: "header-github-link",
+                    "aria-label": "GitHub Repository",
+                }
+            ],
+        },
+        docs: {
+            sidebar: {
+                hideable: true,
+            },
+        },
+        footer: {
+            style: "dark",
+            links: [],
+            copyright: `Copyright © ${new Date().getFullYear()} <a href="https://github.com/zyf722">zyf722</a>. 由 Docusaurus 驱动构建。`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
