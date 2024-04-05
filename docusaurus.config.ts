4197c89eeba1fd3c101427a2285a86fdbe2fc2d9
zyf722 <1124982772@qq.com>
2024-04-05 21:18:33 +0800

    ci: add github-actions integration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
     favicon: "img/favicon.ico",
 
     // Set the production url of your site here
-    url: "https://your-docusaurus-site.example.com",
+    url: "https://zyf722.github.io/maxnotes/",
 
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
@@ -20,6 +20,7 @@ const config: Config = {
     // If you aren't using GitHub pages, you don't need these.
     organizationName: "zyf722", // Usually your GitHub org/user name.
     projectName: "max-notes", // Usually your repo name.
+    trailingSlash: false, // Remove trailing slash
 
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
