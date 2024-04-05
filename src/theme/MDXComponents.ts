17747801601d0c0970cd551d0d155d1f7b9407c9
zyf722 <1124982772@qq.com>
2024-04-05 19:25:15 +0800

    feat: integrate fa-regular icons

--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -5,8 +5,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 
-library.add(fab, fas); // Add all icons to the library so you can use them without importing them individually.
+library.add(fab, fas, far); // Add all icons to the library so you can use them without importing them individually.
 
 export default {
     // Re-use the default mapping
