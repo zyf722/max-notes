3a6cf343ac31f92d74b6c48af98a4f888b2b6062
zyf722 <1124982772@qq.com>
2024-08-13 00:34:25 +0800

    chore: change import of react

--- src/components/Quiz/index.tsx
+++ src/components/Quiz/index.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import { ReactNode, Fragment } from "react";
 import Heading from "@theme/Heading";
 
 type QuizProp = {
-    children: React.ReactNode;
+    children: ReactNode;
     question: string;
 };
 
 export default function Quiz({ children, question }: QuizProp): JSX.Element {
     return (
-        <React.Fragment>
+        <Fragment>
             <Heading as="h3">{question}</Heading>
             <div style={{ display: "flex" }}>
                 <div style={{ fontWeight: "bold" }}>答：</div>
                 <div style={{ width: "100%" }}>{children}</div>
             </div>
-        </React.Fragment>
+        </Fragment>
     );
 }
